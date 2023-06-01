52dc01efcd6c0120718c5f4c9deab7a4514aee79
Israel Rocha <israel.rexc@yahoo.com>
2023-06-01 16:13:32 -0300

    Recording models from db to admin-django page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Documentos, Instituicao, Usuario, Cadastro, CadastroVersao
 
 # Register your models here.
+
+admin.site.register(Documentos)
+admin.site.register(Instituicao)
+admin.site.register(Usuario)
+admin.site.register(Cadastro)
+admin.site.register(CadastroVersao)
