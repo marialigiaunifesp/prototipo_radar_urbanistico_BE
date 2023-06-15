91999e5555e21ee7c9872ca524e14eba2f383fb7
Israel Rocha <israel.rexc@yahoo.com>
2023-06-15 16:22:40 -0300

    Validating 'at least one field must exist' for docs

--- api/serializers.py
+++ api/serializers.py
@@ -11,11 +11,21 @@ class BoletimOficialSerializer(serializers.ModelSerializer):
 		model = BoletimOficial
 		fields = '__all__'
 
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data
+
 # Conhecimento do lugar
 class ConhecimentoLugarSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ConhecimentoLugar
 		fields = '__all__'
+
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data
 
 # Contrato compra venda 
 class ContratoSerializer(serializers.ModelSerializer):
@@ -34,11 +44,21 @@ class DatageoSerializer(serializers.ModelSerializer):
 		model = Datageo
 		fields = '__all__'
 
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data
+
 # Datageo ambiente
 class DatageoAmbienteSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = DatageoAmbiente
 		fields = '__all__'
+
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data
 
 # Diversas Fontes
 class DiversasFontesSerializer(serializers.ModelSerializer):
@@ -46,23 +66,42 @@ class DiversasFontesSerializer(serializers.ModelSerializer):
 		model = DiversasFontes
 		fields = '__all__'
 
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data
+
 # Ficha de cadastramento socioeconomico
 class FichaSocioeconomicoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = FichaSocioeconomico
 		fields = '__all__'
 
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data
 # IBGE
 class IbgeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Ibge
 		fields = '__all__'
 
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data	
+
 # Legislacao
 class LegislacaoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Legislacao
 		fields = '__all__'
+
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data
 
 # Matricula do imovel
 class MatriculaSerializer(serializers.ModelSerializer):
@@ -92,17 +131,32 @@ class ProcessoAdministrativoSerializer(serializers.ModelSerializer):
 		model = ProcessoAdministrativo
 		fields = '__all__'
 
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data
+
 # Processo Judicial
 class ProcessoJudicialSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ProcessoJudicial
 		fields = '__all__'
 
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data
+
 # Vistoria
 class VistoriaSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Vistoria
 		fields = '__all__'
+
+	def validate(self, data):
+		if not data:
+			raise serializers.ValidationError("Must include at least one field")
+		return data
 
 
 '''
@@ -113,7 +167,7 @@ class FormSerializer(serializers.Serializer):
 	dataDocumento = serializers.CharField()
 	referencia = serializers.CharField()
 
-	# Nested
+	# Nested datatypes
 	boletim_oficial = BoletimOficialSerializer(required = False)
 	conhecimento_lugar = ConhecimentoLugarSerializer(required = False)
 	contrato_compra_venda = ContratoSerializer(required = False)
