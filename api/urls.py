a6408bfd9ef9d9475d6b95156e412d50dbf8be27
Israel Rocha <israel.rexc@yahoo.com>
2023-07-05 11:42:57 -0300

    Uploading and downloading files

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 	path('form-create/', views.formCreate, name = "form-create"),
 	path('get-sicar/', views.getSicar, name = "get-sicar"),
-	path('form-create-file/', views.formFile, name = 'form-create-file')
+	path('form-create-file/', views.formFile, name = 'form-create-file'),
+	path('form-get-file/<int:id_arquivo>/', views.download_arquivo, name='download_arquivo'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,15 @@
 from django.views.decorators.csrf import csrf_exempt
+from django.http import FileResponse
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser, MultiPartParser
 from rest_framework import status
-from .serializers import FormSerializer, SicarSerializer, FileSerializer
+from .custom_serializers import FormSerializer, SicarSerializer, FileSerializer
 import io
 from .models import *
 from django.core.serializers import serialize
 import json
+from django.shortcuts import get_object_or_404 
 
 @api_view(['POST'])
 @csrf_exempt
@@ -26,13 +28,23 @@ def formCreate(request):
 def formFile(request):
 	serializer = FileSerializer(data = request.data)
 	if 'arquivo' in request.FILES:
-		print('its')
+		print(request.FILES['arquivo'])
+
 	if(serializer.is_valid()):
 		obj = serializer.save()
 		return Response(serializer.data, status = status.HTTP_201_CREATED)
-	
 	else:
+		print(serializer.errors)
 		return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+def download_arquivo(request, id_arquivo):
+    arquivo = get_object_or_404(Arquivo, id_arquivo=id_arquivo)
+    response = FileResponse(arquivo.arquivo)
+    response['Content-Disposition'] = f'attachment; filename="{arquivo}"'
+    with open('file.ext', 'wb') as f:
+    	f.write(arquivo.arquivo)
+    return response
 
 
 @api_view(['GET'])
