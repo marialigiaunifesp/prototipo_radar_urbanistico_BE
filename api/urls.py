267097584340efd8a8e5a040f9733fa78c0b8c1b
Israel Rocha <israel.rexc@yahoo.com>
2024-03-20 15:22:01 -0300

    Add sicar to db url

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 	path('form-get-info-api/<int:id_sicar>/', views.DocumentoView.as_view(), name='get-info-api'),
 	path('form-get-file/<int:id_arquivo>/', views.download_arquivo, name='download_arquivo'),
 	path('token/', views.token, name = "token"),
+    path('post-sicar/', views.postSicar, name="post-sicar"),
 ]
